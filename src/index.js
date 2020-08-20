7d47439313ac4eaafac624dc9ebba268a872c15f
kiskee <53805976+kiskee@users.noreply.github.com>
2020-08-19 20:00:46 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const Server = http.createServer(app);
+
+//settings
+app.set('port', process.env.PORT || 8080);
+app.set('json spaces', 2);
+
+//middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//routes
+// app.use('/api/inmuebles', require('./routes/inmuebles'));
+app.use(express.static('public'))
+
+// starts the server
+Server.listen(app.get('port'), () => {
+  console.log(`Server on port ${app.get('port')}`);
+});
